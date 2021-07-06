a3fde8d8216ea241f8e3cb6aa2a3f9dcd4393019
harrisonBarnett <h.alexander.barnett@gmail.com>
2021-07-06 17:11:38 -0400

    Testing imports; project structuring

---
+++ src/displaySite.js
@@ -0,0 +1,7 @@
+function box() {
+    const box = document.createElement('div');
+    box.style = "width: 100px; height: 100px; background: red;";
+    return box;
+}
+
+module.exports = {box};
--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log("hello restaurant page");
+const content = document.getElementById('content');
+const title = document.createElement('h1');
+title.innerText = "HELLO RESTAURANT";
+content.appendChild(title);
+
+const displaySite = require('./displaySite');
+
+const box = displaySite.box();
+content.appendChild(box);
+
