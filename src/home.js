7385d1f8128096e84e890faad915440294e567af
harrisonBarnett <h.alexander.barnett@gmail.com>
2021-07-07 00:30:57 -0400

    Fixing styles; separating out display fxns

--- src/home.js
+++ src/home.js
@@ -1,15 +1,36 @@
+function buildPage() {
+    const content = document.getElementById('content');
+    const currentPage = document.querySelector('.page-container');
+
+    if(currentPage) {
+        content.removeChild(currentPage);
+    }
+
+    const pageContainer = document.createElement('div');
+    pageContainer.classList.add('home-page-container');
+    const heroImg = document.createElement('div');
+    heroImg.classList.add('hero-img');
+    pageContainer.appendChild(heroImg);
+
+
+    content.appendChild(pageContainer);
+}
 
 function displayHome() {
     const content = document.getElementById('content');
+    // swap tabs
     const tab = document.getElementById('home-tab');
 
-    const active = document.querySelector('.tab-active');
-    if(!active) {
+    const activeTab = document.querySelector('.tab-active');
+    if(!activeTab) {
         tab.classList.add('tab-active');
     } else {
-    active.classList.remove('tab-active');
+    activeTab.classList.remove('tab-active');
     tab.classList.add('tab-active');
     }
+
+    buildPage();
 }
+
 
 export {displayHome};
--- src/victurals.js
+++ src/victurals.js
@@ -1,15 +1,33 @@
+function buildPage() {
+    const content = document.getElementById('content');
+    const currentPage = document.querySelector('.page-container');
+
+    if(currentPage) {
+        content.removeChild(currentPage);
+    }
+
+    const pageContainer = document.createElement('div');
+    pageContainer.classList.add('victurals-page-container');
+    pageContainer.innerText = "victurals";
+
+
+    content.appendChild(pageContainer);
+}
 
 function displayVicturals() {
     const content = document.getElementById('content');
+    // swap tabs
     const tab = document.getElementById('victurals-tab');
 
-    const active = document.querySelector('.tab-active');
-    if(!active) {
+    const activeTab = document.querySelector('.tab-active');
+    if(!activeTab) {
         tab.classList.add('tab-active');
     } else {
-    active.classList.remove('tab-active');
+    activeTab.classList.remove('tab-active');
     tab.classList.add('tab-active');
-        }
+    }
+
+    buildPage();
 }
 
 export {displayVicturals};
